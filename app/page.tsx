56787ea4e5a54db7118ef9965c6dcec4429aeb62
Robbi Wafik <robbiwafik31@gmail.com>
2024-05-02 19:51:37 +0700

    Set up Radix UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@radix-ui/themes'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div>Homepage</div>
+    <div>
+      <Button>Click me</Button>
+    </div>
   )
 }
