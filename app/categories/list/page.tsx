572c86131cf4ae09b2bdd18a02342534d96bbae7
Robbi Wafik <robbiwafik31@gmail.com>
2024-05-04 05:10:12 +0700

    Build CategoriesPage

--- app/categories/list/page.tsx
+++ app/categories/list/page.tsx
@@ -1,12 +1,37 @@
-import { Button, Text } from '@radix-ui/themes'
+import prisma from '@/prisma/client'
+import { Box, Button, Flex, Table } from '@radix-ui/themes'
 import Link from 'next/link'
 
-export default function CategoriesPage() {
+export default async function CategoriesPage() {
+    const categories = await prisma.category.findMany()
+
     return (
-        <Button>
-            <Text>
-                <Link href={'/categories/new'}>Add category</Link>
-            </Text>
-        </Button>
+        <Box>
+            <Flex justify='end' mb='4'>
+                <Button>
+                    <Link href='/categories/new'>New category</Link>
+                </Button>
+            </Flex>
+            <Table.Root variant='surface'>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.ColumnHeaderCell>#</Table.ColumnHeaderCell>
+                        <Table.ColumnHeaderCell>Name</Table.ColumnHeaderCell>
+                        <Table.ColumnHeaderCell className='hidden md:table-cell'>Created at</Table.ColumnHeaderCell>
+                        <Table.ColumnHeaderCell className='hidden md:table-cell'>Updated at</Table.ColumnHeaderCell>
+                    </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                    {categories.map((category, index) => (
+                        <Table.Row key={category.id}>
+                            <Table.Cell>{index + 1}</Table.Cell>
+                            <Table.RowHeaderCell>{category.name}</Table.RowHeaderCell>
+                            <Table.Cell className='hidden md:table-cell'>{category.createdAt.toDateString()}</Table.Cell>
+                            <Table.Cell className='hidden md:table-cell'>{category.updatedAt.toDateString()}</Table.Cell>
+                        </Table.Row>
+                    ))}
+                </Table.Body>
+            </Table.Root>
+        </Box>
     )
 }
