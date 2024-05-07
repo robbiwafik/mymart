5a969b15534daf7ed3407d2b0adf1a92f243f657
Robbi Wafik <robbiwafik31@gmail.com>
2024-05-07 22:13:16 +0700

    Refactor: extract category table to a component

---
+++ app/categories/list/CategoryTable.tsx
@@ -0,0 +1,38 @@
+import { Category } from '@prisma/client'
+import { Link as RadixLink, Table } from '@radix-ui/themes'
+import Link from 'next/link'
+
+interface Props {
+    categories: Category[],
+    currentPage: number,
+    pageSize: number
+}
+
+export default function CategoryTable({ categories, currentPage, pageSize }: Props) {
+    return (
+        <Table.Root variant='surface'>
+            <Table.Header>
+                <Table.Row>
+                    <Table.ColumnHeaderCell>#</Table.ColumnHeaderCell>
+                    <Table.ColumnHeaderCell>Name</Table.ColumnHeaderCell>
+                    <Table.ColumnHeaderCell className='hidden md:table-cell'>Created at</Table.ColumnHeaderCell>
+                    <Table.ColumnHeaderCell className='hidden md:table-cell'>Updated at</Table.ColumnHeaderCell>
+                </Table.Row>
+            </Table.Header>
+            <Table.Body>
+                {categories.map((category, index) => (
+                    <Table.Row key={category.id}>
+                        <Table.Cell>{(currentPage - 1) * pageSize + index + 1}</Table.Cell>
+                        <Table.RowHeaderCell>
+                            <Link href={`/categories/${category.id}`} legacyBehavior passHref>
+                                <RadixLink>{category.name}</RadixLink>
+                            </Link>
+                        </Table.RowHeaderCell>
+                        <Table.Cell className='hidden md:table-cell'>{category.createdAt.toDateString()}</Table.Cell>
+                        <Table.Cell className='hidden md:table-cell'>{category.updatedAt.toDateString()}</Table.Cell>
+                    </Table.Row>
+                ))}
+            </Table.Body>
+        </Table.Root>
+    )
+}
--- app/categories/list/page.tsx
+++ app/categories/list/page.tsx
@@ -1,7 +1,8 @@
 import prisma from '@/prisma/client'
 import { Prisma } from '@prisma/client'
-import { Box, Button, Flex, Link as RadixLink, Table } from '@radix-ui/themes'
+import { Box, Button, Flex } from '@radix-ui/themes'
 import Link from 'next/link'
+import CategoryTable from './CategoryTable'
 import Pagination from './Pagination'
 import SearchBox from './SearchBox'
 
@@ -10,7 +11,6 @@ interface Props {
 }
 
 export default async function CategoriesPage({ searchParams }: Props) {
-    // Pagination
     const pageSize = 10   
     const currentPage = parseInt(searchParams.page) || 1
 
@@ -19,49 +19,31 @@ export default async function CategoriesPage({ searchParams }: Props) {
             contains: searchParams.search
         }
     }
-
     const categories = await prisma.category.findMany({
         skip: (currentPage - 1) * pageSize,
         take: pageSize,
         where: whereOption
     })
-
     const categoryCount = await prisma.category.count({
         where: whereOption
     })
         
     return (
         <Box>
-            <Flex mb='4' gap='4' justify='between' align='center'>
+            <Flex 
+                mb='4' gap='4' 
+                justify='between' align='center'
+            >
                 <SearchBox />
                 <Button>
                     <Link href='/categories/new'>New category</Link>
                 </Button>
             </Flex>
-            <Table.Root variant='surface'>
-                <Table.Header>
-                    <Table.Row>
-                        <Table.ColumnHeaderCell>#</Table.ColumnHeaderCell>
-                        <Table.ColumnHeaderCell>Name</Table.ColumnHeaderCell>
-                        <Table.ColumnHeaderCell className='hidden md:table-cell'>Created at</Table.ColumnHeaderCell>
-                        <Table.ColumnHeaderCell className='hidden md:table-cell'>Updated at</Table.ColumnHeaderCell>
-                    </Table.Row>
-                </Table.Header>
-                <Table.Body>
-                    {categories.map((category, index) => (
-                        <Table.Row key={category.id}>
-                            <Table.Cell>{(currentPage - 1) * pageSize + index + 1}</Table.Cell>
-                            <Table.RowHeaderCell>
-                                <Link href={`/categories/${category.id}`} legacyBehavior passHref>
-                                    <RadixLink>{category.name}</RadixLink>
-                                </Link>
-                            </Table.RowHeaderCell>
-                            <Table.Cell className='hidden md:table-cell'>{category.createdAt.toDateString()}</Table.Cell>
-                            <Table.Cell className='hidden md:table-cell'>{category.updatedAt.toDateString()}</Table.Cell>
-                        </Table.Row>
-                    ))}
-                </Table.Body>
-            </Table.Root>
+            <CategoryTable 
+                categories={categories}
+                currentPage={currentPage} 
+                pageSize={pageSize} 
+            />
             <Pagination 
                 categoryCount={categoryCount}
                 currentPage={currentPage} 
