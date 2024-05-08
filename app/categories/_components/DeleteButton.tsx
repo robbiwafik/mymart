3cf6f0aa79157e9cd2d96415cab13fe97eb4d308
Robbi Wafik <robbiwafik31@gmail.com>
2024-05-09 05:38:34 +0700

    Disable the delete category button when a deletion process is in progress

--- app/categories/_components/DeleteButton.tsx
+++ app/categories/_components/DeleteButton.tsx
@@ -1,7 +1,8 @@
 import paths from '@/app/paths'
-import { AlertDialog, Button, Flex } from '@radix-ui/themes'
+import { AlertDialog, Button, Flex, Spinner } from '@radix-ui/themes'
 import axios from 'axios'
 import { useRouter } from 'next/navigation'
+import { useState } from 'react'
 import { toast, ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
@@ -11,15 +12,18 @@ interface Props {
 
 export default function DeleteButton({ categoryId }: Props) {
     const router = useRouter()
+    const [ isDeleting, setIsDeleting ] = useState(false)
     
     const handleDelete = async () => {
         try {
+            setIsDeleting(true)
             await axios.delete(`/api/categories/${categoryId}`)
             router.push(paths.CATEGORY_LIST)
             router.refresh()
         }
         catch {
             toast.error('Unexpected error ocurred.')
+            setIsDeleting(false)
         }
     }
 
@@ -27,7 +31,10 @@ export default function DeleteButton({ categoryId }: Props) {
         <>
             <AlertDialog.Root>
                 <AlertDialog.Trigger>
-                    <Button color='red'>Delete</Button>
+                    <Button color='red' disabled={isDeleting}>
+                        Delete
+                        {isDeleting && <Spinner />}
+                    </Button>
                 </AlertDialog.Trigger>
                 <AlertDialog.Content maxWidth='450px'>
                     <AlertDialog.Title>Delete category</AlertDialog.Title>
@@ -37,7 +44,9 @@ export default function DeleteButton({ categoryId }: Props) {
                             <Button variant='soft' color='gray'>Cancel</Button>
                         </AlertDialog.Cancel>
                         <AlertDialog.Action>
-                            <Button onClick={handleDelete} color='red'>Delete</Button>
+                            <Button onClick={handleDelete} color='red'>
+                                Delete
+                            </Button>
                         </AlertDialog.Action>
                     </Flex>
                 </AlertDialog.Content>
