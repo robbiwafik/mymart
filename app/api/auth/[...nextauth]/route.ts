d49e163319b0ccf3f81f40769ebbb0faaa606fb2
Robbi Wafik <robbiwafik31@gmail.com>
2024-05-10 14:13:59 +0700

    Configure NextAuth CredentialsProvider

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,38 @@
+import prisma from '@/prisma/client'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import { SHA256 as sha256 } from 'crypto-js'
 import NextAuth from 'next-auth'
+import CredentialsProvider from 'next-auth/providers/credentials'
 
 const handler = NextAuth({
-    providers: []
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        CredentialsProvider({
+            name: 'Credentials',
+            credentials: {
+                username: { label: 'Username', placeholder: 'Username'},
+                password: { label: 'Password', type: 'password'}
+            },
+            async authorize(credentials, req) {
+                if (!credentials?.username || !credentials?.password)
+                    return null
+
+                const user = await prisma.user.findUnique({
+                    where: {
+                        username: credentials.username
+                    }
+                })
+
+                if (!user)
+                    return null
+
+                return sha256(credentials.password).toString() === user.password ?
+                    user : null
+            }
+        }),
+    ],
+    session: { strategy: 'jwt' }
 })
 
-export { handler as GET, handler as POST}
+export { handler as GET, handler as POST }
+
