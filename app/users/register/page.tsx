ee1673cedae25ab1c3d87aaffea78d076f4076df
Robbi Wafik <robbiwafik31@gmail.com>
2024-05-11 13:49:50 +0700

    Show submit status message on the register form

--- app/users/register/page.tsx
+++ app/users/register/page.tsx
@@ -1,36 +1,43 @@
 'use client'
 
 import Field from '@/app/components/Field'
+import paths from '@/app/paths'
 import { registerUserSchema } from '@/app/validationSchema'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { Box, Button, Card, Flex, Spinner, Text } from '@radix-ui/themes'
-import axios from 'axios'
+import { CheckIcon, InfoCircledIcon } from '@radix-ui/react-icons'
+import { Box, Button, Callout, Card, Flex, Link as RadixLink, Spinner, Text } from '@radix-ui/themes'
+import axios, { isAxiosError } from 'axios'
+import Link from 'next/link'
+import { useState } from 'react'
 import { SubmitHandler, useForm } from 'react-hook-form'
 import { z } from 'zod'
-import { ToastContainer, toast } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
-import { useState } from 'react'
 
 type Inputs = z.infer<typeof registerUserSchema>
 
 export default function RegisterPage() {
     const [ isSubmitting, setIsSubmitting ] = useState(false)
-    const { register, handleSubmit, formState: { errors } } = useForm<Inputs>({
+    const [ isRegistered, setIsRegistered ] = useState(false)
+    const [ onSubmitError, setOnSubmitError ] = useState('')
+    const { register, handleSubmit, reset, formState: { errors } } = useForm<Inputs>({
         resolver: zodResolver(registerUserSchema)
     }) 
 
     const onSubmit: SubmitHandler<Inputs> = async (values) => {
         try {
             setIsSubmitting(true)
-            const user = await axios.post('/api/users', values)
-            // sign in a user
-            // redirect them to main page
-            setIsSubmitting(false) //TODO: remove when redirecting users to the main page
+            await axios.post('/api/users', values)
+            setIsRegistered(true)
+            setOnSubmitError('')
+            reset()
         }
-        catch {
-            setIsSubmitting(false)
-            toast.error('Unexpected error ocurred')
+        catch (error) {
+            if (isAxiosError(error))
+                setOnSubmitError(error.response?.data.error)
+            else
+                setOnSubmitError('Unexpected error ocurred.')
         }
+
+        setIsSubmitting(false)
     }
     
     return (
@@ -38,14 +45,29 @@ export default function RegisterPage() {
             <Card size='3'>
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <Box width='400px'>
-                        <Text
-                            as='p' 
-                            align='center' 
-                            size='6' 
-                            mb='4'
-                        >
+                        <Text as='p' align='center' size='6' mb='4'>
                             Register Form
                         </Text>
+                        {isRegistered && 
+                        <Callout.Root my='4'>
+                            <Callout.Icon>
+                                <CheckIcon />
+                            </Callout.Icon>
+                            <Callout.Text>
+                                {`Your account was successfully created. Click `}
+                                <Link href={paths.LOGIN_PAGE} passHref legacyBehavior>
+                                    <RadixLink>here</RadixLink>
+                                </Link>
+                                {` to go back to the Sign in form!`}
+                            </Callout.Text>
+                        </Callout.Root>}
+                        {onSubmitError &&
+                        <Callout.Root my='4' color='red'>
+                            <Callout.Icon>
+                                <InfoCircledIcon />
+                            </Callout.Icon>
+                            <Callout.Text>{onSubmitError}</Callout.Text>
+                        </Callout.Root>}
                         <Field 
                             error={errors.name?.message} 
                             label='Name' 
@@ -69,7 +91,6 @@ export default function RegisterPage() {
                     </Box>
                 </form>
             </Card>
-            <ToastContainer />
         </Flex>
     )
 }
